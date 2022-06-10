9aaade0b25922d038ad76aa6673abda5485284c1
sajidhasan <md.sajidhasan@yahoo.com>
2022-06-10 16:41:59 +0100

    Initial Commit

---
+++ JPMCMCoEUITests/JPMCMCoEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JPMCMCoEUITestsLaunchTests.swift
+//  JPMCMCoEUITests
+//
+//  Created by Sajid Hasan on 10/06/2022.
+//
+
+import XCTest
+
+class JPMCMCoEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
